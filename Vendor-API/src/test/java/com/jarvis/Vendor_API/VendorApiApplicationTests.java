828fda3eb7a8d8dc2a2c3b52e241d635bfe36bc1
JARVIS3954P <gaintacolyte9@gmail.com>
2025-02-03 03:20:42 +0530

    Initial Commit

---
+++ Vendor-API/src/test/java/com/jarvis/Vendor_API/VendorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jarvis.Vendor_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
